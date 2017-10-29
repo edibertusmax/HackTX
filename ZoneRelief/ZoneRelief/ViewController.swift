0badd013dff49e41915d8e3c43d4ec2c9947ce6e
ep23934 <e.pineda@utexas.edu>
2017-10-29 03:26:12 -0500

    Obtained data from twitter json application, need to authorize the connection

--- ZoneRelief/ZoneRelief/ViewController.swift
+++ ZoneRelief/ZoneRelief/ViewController.swift
@@ -14,25 +14,19 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
   
   var locationManager = CLLocationManager()
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view, typically from a nib.
-  /*  let camera = GMSCameraPosition.camera(withLatitude: -12.5420, longitude: 27.8546, zoom: 10.0)
-    let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-    view = mapView*/
-    
-    // Creates a marker in the center of the map.
-    
+    // This how we'll add the coordinates for people in distress
     /*let reliefCircle = GMSCircle()
-    reliefCircle.position = CLLocationCoordinate2D(latitude: -12.5420, longitude: 27.8546)
-    reliefCircle.radius = 5000
-    reliefCircle.strokeColor = UIColor.red
-    reliefCircle.fillColor = UIColor.red.withAlphaComponent(0.5)
-    //marker.title = "Sydney"
-    //marker.snippet = "Australia"
-    reliefCircle.map = mapView*/
+     reliefCircle.position = CLLocationCoordinate2D(latitude: -12.5420, longitude: 27.8546)
+     reliefCircle.radius = 5000
+     reliefCircle.strokeColor = UIColor.red
+     reliefCircle.fillColor = UIColor.red.withAlphaComponent(0.5)
+     //marker.title = "Sydney"
+     //marker.snippet = "Australia"
+     reliefCircle.map = mapView*/
     
     // Ask for Authorisation from the User.
     self.locationManager.requestAlwaysAuthorization()
@@ -49,32 +43,75 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
     view = mapView
     
-    
-    /*
-    locationManager = CLLocationManager()
-    locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    locationManager.requestAlwaysAuthorization()
-    locationManager.distanceFilter = 50
-    locationManager.startUpdatingLocation()
-    locationManager.delegate = self*/
     let marker = GMSMarker()
     marker.position = CLLocationCoordinate2D(latitude: locationManager.location!.coordinate.latitude, longitude: locationManager.location!.coordinate.longitude)
     marker.map = mapView
     
+    getTweets()
+    
     
     
   }
+
+   /* Referenced from https://grokswift.com/simple-rest-with-swift/ */
+   func getTweets() {
+   let search:String = "https://api.twitter.com/1.1/search/tweets.json?q=%23zonerelief";
+   guard let searchURL = URL(string: search) else {
+   print("Error: cannot create URL")
+   return
+   }
+   let urlRequest = URLRequest(url: searchURL)
+   let session = URLSession.shared
+   let task = session.dataTask(with: urlRequest) {
+   (data, response, error) in
+   // check for any errors
+   guard error == nil else {
+   print("error calling GET on /todos/1")
+   print(error!)
+   return
+   }
+   // make sure we got data
+   guard let responseData = data else {
+   print("Error: did not receive data")
+   return
+   }
+   // parse the result as JSON, since that's what the API provides
+   do {
+   guard let todo = try JSONSerialization.jsonObject(with: responseData, options: [])
+   as? [String: Any] else {
+   print("error trying to convert data to JSON")
+   return
+   }
+   // now we have the todo
+   // let's just print it to prove we can access it
+   print("The todo is: " + todo.description)
+   
+   // the todo object is a dictionary
+   // so we just access the title using the "title" key
+   // so check for a title and print it if we have one
+   guard let todoTitle = todo["title"] as? String else {
+   print("Could not get todo title from JSON")
+   return
+   }
+   print("The title is: " + todoTitle)
+   } catch  {
+   print("error trying to convert data to JSON")
+   return
+   }
+   }
+   task.resume()
+   }
   
   func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     var locValue:CLLocationCoordinate2D = manager.location!.coordinate
     print("locations = \(locValue.latitude) \(locValue.longitude)")
   }
-
-
+  
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
+  
 }
 
